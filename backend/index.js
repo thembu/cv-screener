ca0f9d9f29d45a1eae26da6a4a502f93571a7f83
thembu <rakhivhanithembu@gmail.com>
2026-04-21 16:33:29 +0200

    added vite frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const screenerRoutes = require('./routes/screener');
+const cors = require('cors');   
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use (cors());
 
 app.use('/api/screener', screenerRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
